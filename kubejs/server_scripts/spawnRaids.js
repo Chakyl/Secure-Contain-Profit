f07889857a91c39e828a27a629b608e87708e504
Chakyl <chakyl1127@gmail.com>
2026-09-25 20:50:07 -0400

    configured

--- kubejs/server_scripts/spawnRaids.js
+++ kubejs/server_scripts/spawnRaids.js
@@ -6,9 +6,7 @@ PlayerEvents.tick((e) => {
     let day = global.getDay(level) - 1;
     if (!player.persistentData.dayLastRaided) player.persistentData.dayLastRaided = -1;
     if (day > 5 && day % 15 == 0 && global.compareDay(day, player.persistentData.getInt("dayLastRaided"), 1)) {
-        if (Gateway.createGateway(level, "gateways:verdant_raid", player.x, player.y + 2, player.z, player)) {
-            player.persistentData.dayLastRaided = day;
-        }
-
+        server.runCommandSilent(`open_gateway ${player.username} gateways:verdant_raid`)
+        player.persistentData.dayLastRaided = day;
     }
 })
